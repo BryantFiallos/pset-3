9933a1eb8e8f9a1cb158144fe010188545d672f1
unknown <bryant.fiallos@gmail.com>
2020-09-15 15:23:19 -0400

    finished 2 and 3

---
+++ src/Exercise3.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Exercise3 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Height : ");
+        double height = input.nextDouble();
+
+        System.out.print("Width : ");
+        double width = input.nextDouble();
+
+        double area = (height * width);
+
+        double areaMM = area * 645.16;
+
+        System.out.println("");
+
+        System.out.printf("%,.2f", areaMM);
+        System.out.print(" square millimeters.");
+    }
+}
