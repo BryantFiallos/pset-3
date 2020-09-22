7918e4db35f71e58c097040e2a69268bcd3053dd
unknown <bryant.fiallos@gmail.com>
2020-09-22 14:32:27 -0400

    finished 9

--- src/Exercise9.java
+++ src/Exercise9.java
@@ -5,29 +5,33 @@ public class Exercise9 {
         Scanner input = new Scanner(System.in);
 
         System.out.print("Amount : ");
-        double amount = input.nextDouble();
+        float userInput = input.nextFloat();
 
-        double quarters = amount / 0.25;
+        userInput = userInput * 100;
+
+        int amount = (int)userInput;
+
+        int quarters = amount / 25;
         quarters = quarters - (quarters % 1);
-        amount = amount - (quarters * 0.25);
-        double dimes = amount / 0.10;
+        amount = amount - (quarters * 25);
+        int dimes = amount / 10;
         dimes = dimes - (dimes % 1);
-        amount = amount - (dimes * 0.10);
-        double nickels = amount / 0.05;
+        amount = amount - (dimes * 10);
+        int nickels = amount / 5;
         nickels = nickels - (nickels % 1);
-        amount = amount - (nickels * 0.05);
-        double pennies = amount / 0.01;
+        amount = amount - (nickels * 5);
+        int pennies = amount / 1;
         pennies = pennies - (pennies % 1);
 
 
         System.out.print("\nQuarters : ");
-        System.out.printf("%.0f", quarters);
+        System.out.print(quarters);
         System.out.print("\nDimes    : ");
-        System.out.printf("%.0f", dimes);
+        System.out.print(dimes);
         System.out.print("\nNickels  : ");
-        System.out.printf("%.0f", nickels);
+        System.out.print(nickels);
         System.out.print("\nPennies  : ");
-        System.out.printf("%.0f", pennies);
+        System.out.print(pennies);
 
     }
 }
